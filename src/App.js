460d82eb74ee4f5d57e17ad499ed815120a7b36a
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-09-28 11:31:29 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(resData => setUsers(resData.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <table>
+        <tbody>
+        {
+          users.map((user, index) => 
+           <tr key={index}>
+             <td>{user.first_name}</td>
+             <td>{user.last_name}</td>
+             <td>{user.email}</td>
+             <td><img alt="avatar" src={user.avatar} /></td>
+           </tr>
+          )
+        }
+        </tbody>
+      </table>
+     </div>
   );
 }
 
